e72ecc4cf80885a2c570c89f3979e7a229738bbb
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-05-07 10:34:10 +0900

    make basic operators functions

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+
+function multiply(num1,num2) {
+    return num1*num2;
+}
+
+function divide(num1,num2) {
+    return num1/num2;
+}
+
+console.log(add(4,5));
+console.log(subtract(4,5));
+console.log(multiply(4,5));
+console.log(divide(4,5));
---
+++ readme.md
@@ -0,0 +1,6 @@
+1. Calculator is going to contain functions for all of the basic math operators you typically find on calculators. 
+Start by creating functions for the following items and testing them in your browser's console.
+- add 
+- subtract
+- multiply
+- divide
